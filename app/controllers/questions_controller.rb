f794af216f79ec48b341566cebff510b76a9d024
andrew hulme <andrew_hulme04@hotmail.com>
2024-03-13 21:15:24 +1100

    Implemented main funtionality.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,10 +10,10 @@ class QuestionsController < ApplicationController
     @questions = params[:question]
     if @questions.include?("?")
       @answer =  "Silly question, get dressed and go to work!"
-    # elsif @questions == "I am going to work"
-    #   return ""
-    # else
-
+    elsif @questions == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
